5fce197fb1d10788b6d96f3e2588de93dd4321e6
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2022-05-19 16:34:04 +0300

    feat: done task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(__dirname, (err, files) => {
+  if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+      if(file === 'secret-folder'){
+        fs.readdir(path.join(__dirname, 'secret-folder'),{ withFileTypes: true }, (err, files) => {
+          if (err)
+            console.log(err);
+          else {
+            console.log('\nDirectory filenames:');
+            files.forEach(file => {
+              if(file.isFile()){
+                fs.stat(path.join(__dirname,'secret-folder',file.name), (err, stats) => {
+                  if(err) throw err;
+                  console.log(`${path.basename(file.name, path.extname(file.name))} - ${path.extname(file.name).slice(1)} - ${(stats.size) / 1000}Kb`);
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
