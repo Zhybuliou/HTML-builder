de7cee8779842b0ee93b6232f223a6c40ef76b44
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2022-05-21 01:39:09 +0300

    fix: task 04-copy-diretory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,38 +2,37 @@ const fs = require('fs');
 const path = require('path');
 
 function copyDir(){
-  fs.readdir(__dirname, (err, files) => {
-    if (err)
-      console.log(err);
-    else {
-      fs.rm(path.join(__dirname, 'files-copy'),{ recursive: true, force: true }, err => {
-        if(err) {console.log(err);}
-      });
-      setTimeout(() => {
-        fs.mkdir(path.join(__dirname, 'files-copy'),{ recursive: true, force: true }, err => {
-          if(err) {console.log(err);}
-        });
+  fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, () => {
+    fs.mkdir(path.join(__dirname, 'files-copy'),{ recursive: true }, err => {
+      if(err) {console.log(err);}
+      fs.readdir(__dirname, (err, files) => {
+        if (err)
+          console.log(err);
+        else {
+          files.forEach(file => {
+            if(file === 'files'){
+              fs.readdir(path.join(__dirname, 'files'),{ withFileTypes: true }, (err, files) => {
+                if (err)
+                  console.log(err);
+                else {
+                  files.forEach(file => {
+                    if(file.isFile()){
+                      fs.copyFile(path.join(__dirname,'files',`${file.name}`), path.join(__dirname,'files-copy',`${file.name}`), (err) =>{
+                        if(err){console.log(err);}
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
     
-        files.forEach(file => {
-          if(file === 'files'){
-            fs.readdir(path.join(__dirname, 'files'),{ withFileTypes: true }, (err, files) => {
-              if (err)
-                console.log(err);
-              else {
-                files.forEach(file => {
-                  if(file.isFile()){
-                    fs.copyFile(path.join(__dirname,'files',`${file.name}`), path.join(__dirname,'files-copy',`${file.name}`), (err) =>{
-                      if(err){console.log(err);}
-                    });
-                  }
-                });
-              }
-            });
-          }
-        });
-      },500);
-
-    }
+    
+        }
+      });
+    });
   });
+
+
 }
 copyDir();
